626a9839573fb6d6f5322da848ba0a806561d4db
徐知新 <18121760625@163.com>
2020-07-02 16:03:01 +0800

    单击主页面的左侧按钮的时候，被单击的都要高亮显示

--- admin/js/index.js
+++ admin/js/index.js
@@ -26,6 +26,27 @@ $(function () {
         window.location.href = './login.html';
     })
 
-    
+    // 主页面左侧按钮高亮显示
+    // 给左侧按钮的每一个div注册事件，每个div都有一个共同的类level01
+    $('.menu .level01').on('click', function () {
+        // 点击当前的div添加active类，其余兄弟要移除active类
+        $(this).addClass('active').siblings().removeClass('active');
+        // 当点击文章管理的时候，要实现一个合并与展开的切换
+        if ($(this).index() === 1) {
+            // 实现切换
+            $('.menu .level02').slideToggle();
+            // 右侧小三角也要进行切换
+            $(this).find('b').toggleClass('rotate0')
+            // 只要展开或是合并文章管理，就要让第一个li标签高亮显示
+            $('.menu .level02 li:eq(0)').trigger('click')
+        }
+    })
+    // 文章管理中的li标签被点击的时候，高亮显示 
+    // 给每一个li标签注册事件
+    $('.menu .level02 li').on('click', function () {
+        // 
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+
 
 })
